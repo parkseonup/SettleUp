ab5d789c102a780aa5530f23c370e0f4f59b3c2a
Seonu <seonup95@gmail.com>
2023-11-06 01:50:40 +0900

    feat: ContentTitle 제목 전달 방식 변경
    - childen으로 전달하던  title 내용을 value prop으로 전달하도록 구현
    - as prop이 컴포넌트일 경우에도 ContentTitle이 정상적으로 동작하도록 구현

--- invoice-project/src/components/ContentTitle/molecules/ContentTitle.tsx
+++ invoice-project/src/components/ContentTitle/molecules/ContentTitle.tsx
@@ -1,27 +1,35 @@
-import { ElementType, ReactNode } from 'react';
+import { ElementType } from 'react';
 import { css } from '@emotion/react';
 import { colors } from '../../../styles/variables/colors';
 import ContentTitleOption from '../atoms/ContentTitleOption';
 
-interface ContentTitleProps {
+interface Props {
   as: ElementType;
+  value: string;
   option?: string;
-  children?: ReactNode;
 }
 
-export default function ContentTitle({ as: Title, option, children }: ContentTitleProps) {
+export default function ContentTitle({ as: Title, value, option }: Props) {
+  const style = css({
+    padding: 0,
+    margin: 0,
+    fontSize: '15px',
+    fontWeight: 500,
+    color: colors.BLACK,
+  });
+  const cssProp = typeof Title === 'function' ? { style } : { css: style };
+
   return (
-    <Title
-      css={css({
-        padding: 0,
-        margin: 0,
-        fontSize: '15px',
-        fontWeight: 500,
-        color: colors.BLACK,
-      })}
-    >
-      {children}
+    <Title {...cssProp}>
+      {value}
       {option ? <ContentTitleOption value={option} /> : null}
     </Title>
   );
 }
+
+/**
+ * Title을 컴포넌트로 전달할 경우, 컴포넌트 선언부 예시
+ * function Label({ children, style }: { children: ReactNode; style: SerializedStyles }) {
+  return <label css={css(style)}>{children}</label>;
+}
+ */
