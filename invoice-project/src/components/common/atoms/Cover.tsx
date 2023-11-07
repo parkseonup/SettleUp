48e6e35a245b8c7b23baf9051632e57127b39cb3
Seonu <seonup95@gmail.com>
2023-11-07 23:40:02 +0900

    feat: Cover 구현
    모달창 뒤의 UI가 클릭되지 않도록 하는 Cover 구현

---
+++ invoice-project/src/components/common/atoms/Cover.tsx
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react';
+import { zIndexes } from '../../../styles/variables/zIndexes';
+import { HTMLAttributes, useEffect } from 'react';
+
+interface Props {
+  isOpen: boolean;
+  onClick?: HTMLAttributes<HTMLDivElement>['onClick'];
+}
+
+export default function Cover({ isOpen, onClick }: Props) {
+  console.log('[cover]');
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? 'hidden' : 'visible';
+  }, [isOpen]);
+
+  return (
+    <div
+      onClick={onClick}
+      css={css({
+        position: 'fixed',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        zIndex: zIndexes.COVER,
+        display: isOpen ? 'block' : 'none',
+        width: '100%',
+        height: '100%',
+      })}
+    ></div>
+  );
+}
