1277d91b804d1b1a6636be84dc2b64e7485353a0
Seonu <seonup95@gmail.com>
2023-11-09 14:46:05 +0900

    feat: getChildrenComponent 함수 구현
    동일한 이름의 컴포넌트가 많을 경우에 사용되는 함수로 getChildComponent와 동일한 반환값을  배열로 반환하는 getChildrenComponent 구현

--- invoice-project/src/utils/getChildComponent.tsx
+++ invoice-project/src/utils/getChildComponent.tsx
@@ -4,3 +4,7 @@ import { jsx } from '@emotion/react';
 export default function getChildComponent(children: ReactNode, target: jsx.JSX.ElementType | ReactNode) {
   return Children.toArray(children).find((child) => isValidElement(child) && child.type === target);
 }
+
+export function getChildrenComponent(children: ReactNode, target: jsx.JSX.ElementType | ReactNode) {
+  return Children.toArray(children).filter((child) => isValidElement(child) && child.type === target);
+}
