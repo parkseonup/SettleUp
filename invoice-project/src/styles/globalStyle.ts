7317182fedcdfc4b713713fd035aca21e1dc9c7a
Seonu <seonup95@gmail.com>
2023-11-10 17:42:39 +0900

    fix: emotion의 font-smoothing 적용방식으로 변경

--- invoice-project/src/styles/globalStyle.ts
+++ invoice-project/src/styles/globalStyle.ts
@@ -6,8 +6,8 @@ export const globalStyle = css(emotionNormalize, {
   body: {
     fontFamily:
       '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif',
-    '-webkit-font-smoothing': 'antialiased',
-    '-moz-osx-font-smoothing': 'grayscale',
+    WebkitFontSmoothing: 'antialiased',
+    MozOsxFontSmoothing: 'grayscale',
   },
 
   '*::before, *::after, body *, body *::before, body *::after': {
