978af265f6619c8a306f0bf893147c88bd4587cf
Seonu <seonup95@gmail.com>
2023-11-11 14:56:35 +0900

    chore: Airbnb eslint 제거 및 rules 수정

--- invoice-project/.eslintrc.cjs
+++ invoice-project/.eslintrc.cjs
@@ -1,28 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, node: true },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/stylistic-type-checked', // @typescript-eslint @v6
-    'plugin:@typescript-eslint/recommended-type-checked', // @typescript-eslint @v6
-  ],
+  env: { browser: true, es2020: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
-  plugins: ['prettier', '@typescript-eslint', 'import', 'react', 'react-refresh'],
+  plugins: ['react-refresh'],
   rules: {
-    'no-console': 'warn',
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': [
+      'warn', { allowConstantExport: true }
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/semi': 'error',
   },
 };
