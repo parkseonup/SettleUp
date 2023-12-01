97ba45a9d57812d37863e45604e63ff62de689d6
Seonu <seonup95@gmail.com>
2023-12-01 14:43:58 +0900

    refactor: InputField의 Input 컴포넌트 props 구체화

--- src/components/InputField/atoms/Input.tsx
+++ src/components/InputField/atoms/Input.tsx
@@ -1,22 +1,28 @@
-import { InputHTMLAttributes } from 'react';
+import { ForwardedRef, InputHTMLAttributes, forwardRef } from 'react';
 import { Style } from '../../../types/Style';
 import { css } from '@emotion/react';
 import { colors } from '../../../styles/variables/colors';
 
 interface Props {
   type?: InputHTMLAttributes<HTMLInputElement>['type'];
+  readOnly?: InputHTMLAttributes<HTMLInputElement>['readOnly'];
   placeholder?: InputHTMLAttributes<HTMLInputElement>['placeholder'];
   isActive?: boolean;
   customStyle?: Style;
-  onFocus?: (arg?: any) => void;
-  onBlur?: (arg?: any) => void;
+  onFocus?: InputHTMLAttributes<HTMLInputElement>['onFocus'];
+  onBlur?: InputHTMLAttributes<HTMLInputElement>['onBlur'];
 }
 
-export default function Input({ type, placeholder, isActive, customStyle, onFocus, onBlur }: Props) {
+export default forwardRef(function Input(
+  { type, readOnly, placeholder, isActive, customStyle, onFocus, onBlur }: Props,
+  ref?: ForwardedRef<HTMLInputElement>,
+) {
   return (
     <input
+      ref={ref}
       type={type ?? 'text'}
       placeholder={placeholder}
+      readOnly={readOnly}
       onFocus={onFocus}
       onBlur={onBlur}
       css={css(
@@ -35,4 +41,4 @@ export default function Input({ type, placeholder, isActive, customStyle, onFocu
       )}
     />
   );
-}
+});
