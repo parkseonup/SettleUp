8e9082fa4d0743fd414c847fef39486cd7402820
Seonu <seonup95@gmail.com>
2023-12-06 00:05:22 +0900

    remove: PageLayout의 Content 제거

--- src/components/pageLayout/atoms/Content.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface ContentProps {
-  children: ReactNode;
-}
-
-export default function Content({ children }: ContentProps) {
-  return <div>{children}</div>;
-}
