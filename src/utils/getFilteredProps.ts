313c19549799cb1fa78477a1de0aebe9231268aa
Seonu <seonup95@gmail.com>
2023-12-05 18:42:23 +0900

    remove: getFilteredProps 파일 제거

--- src/utils/getFilteredProps.ts
+++
@@ -1,9 +0,0 @@
-export default function getFilteredProps(props: { [key: string]: any }, filterList: string[]) {
-  const filteredProps: { [key: string]: any } = {};
-
-  Object.entries(props).forEach(([key, value]) => {
-    if (!filterList.includes(key)) filteredProps[key] = value;
-  });
-
-  return filteredProps;
-}
