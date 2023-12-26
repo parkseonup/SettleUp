9a1bd5abdf8adfb8fc7d6b83358e53f7267e99ad
Seonu <seonup95@gmail.com>
2023-12-26 16:23:52 +0900

    fix: child.type 체크가 불가능했던 문제 해결
    문제 상황
    - 합성 컴포넌트로 구현된 InputField의 children props에서 특정 자식 컴포넌트를 추출할 때, 자식 컴포넌트의 type과 선언된 컴포넌트(target)가 동일한지 확인할 수 없는 에러가 발생
    
    문제 원인
    - Emotion 라이브러리에서는 자체적인 렌더링을 하기 때문에 wrapping된 자식 컴포넌트를 반환함. 때문에 child.type에 접근하면 일반적으로 기대하는 컴포넌트가 아닌 래핑된 컴포넌트이므로, 동일한 컴포넌트로 인지하지 못함 (https://github.com/emotion-js/emotion/issues/1173)
    
    해결 방안
    - 이모션에 의해 렌더링된 컴포넌트일 경우 child.props.__EMOTION_TYPE_PLEASE_DO_NOT_USE__과 비교하는 것으로 해결함
    
    평가
    - 속성명에서 알 수 있듯, 이모션에서는 해당 속성을 사용하지 않기를 바라고 있기 때문에 올바른 방법이라고 생각되지 않음.
    - 하지만, 리액트에서 부모가 자식을 탐색할 수 있도록 제공하고 있는 것을 이모션 라이브러리가 접근하지 못하도록 강제하는 것이 더 좋지 않다고 생각함
    - 이모션 측에서는 11버전에서 자체 렌더링 때문에 발생하는 문제들을 해결하고자 노력 중이라는 입장을 내었으므로, 추후 __EMOTION_TYPE_PLEASE_DO_NOT_USE__ 속성을 사용하지 않도록 마이그레이션 하고자 함.
    - 또는, 이모션이 아닌 styled-component와 같은 다른 CSS-in-JS로 변경하는 것도 고려 중임.

--- src/utils/getChildComponent.tsx
+++ src/utils/getChildComponent.tsx
@@ -1,7 +1,13 @@
-import { Children, ElementType, ReactNode, isValidElement } from 'react';
+import { Children, ElementType, ReactElement, ReactNode, isValidElement } from 'react';
 
-export default function getChildComponent(children: ReactNode, target: ElementType | ReactNode): ReactNode {
-  const child = Children.toArray(children).find((child) => isValidElement(child) && child.type === target);
+export default function getChildComponent(children: ReactNode, target: ElementType): ReactElement | null {
+  const child = Children.toArray(children).find((child) => {
+    if (!isValidElement(child)) return false;
 
-  return child;
+    return child.props?.__EMOTION_TYPE_PLEASE_DO_NOT_USE__
+      ? child.props.__EMOTION_TYPE_PLEASE_DO_NOT_USE__ === target
+      : child.type === target;
+  });
+
+  return isValidElement(child) ? child : null;
 }
