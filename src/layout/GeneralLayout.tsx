cd491269454946ac8b5e495c10983a32d756f5e4
Seonu <seonup95@gmail.com>
2023-12-26 17:22:04 +0900

    refactor: GlobalHeader 구조 변경
    Menu 컴포넌트 추상화에 따른 GlobalHeader 구조 변경

--- src/layout/GeneralLayout.tsx
+++ src/layout/GeneralLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
-import GlobalHeader from '../components/GlobalHeader/organisms/GlobalHeader';
 import { Global } from '@emotion/react';
 import { globalStyle } from '../styles/globalStyle';
+import GlobalHeader from '../components/common/organisms/GlobalHeader';
 
 export default function GeneralLayout({ children }: { children: ReactNode }) {
   return (
