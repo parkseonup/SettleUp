bc193dff54049443278e6bd92986ebd431602bbb
Seonu <seonup95@gmail.com>
2024-01-03 20:02:55 +0900

    rename: Checkbox를 MultiSelect의 Option으로 변경

---
+++ src/components/MultiSelect/atoms/Option.tsx
@@ -0,0 +1,37 @@
+import { colors } from '../../../styles/variables/colors';
+import { visibilityHidden } from '../../../styles/common/display/visibilityHidden';
+import { InputHTMLAttributes } from 'react';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Option({ children, ...props }: Props) {
+  return (
+    <label>
+      <input type="checkbox" css={visibilityHidden} {...props} />
+      <span
+        css={{
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '32px',
+          padding: '0 16px',
+          fontSize: '14px',
+          color: colors.DARK_GRAY,
+          textAlign: 'center',
+          backgroundColor: colors.WHITE,
+          border: `1px solid ${colors.LIGHT_GRAY}`,
+          borderRadius: '100px',
+          cursor: 'pointer',
+
+          'input:checked + &': {
+            color: colors.WHITE,
+            backgroundColor: colors.BLACK,
+            borderColor: colors.BLACK,
+          },
+        }}
+      >
+        {children}
+      </span>
+    </label>
+  );
+}
