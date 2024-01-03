2130ca0440c5e20743c599ce5f9eeb45a692bfbe
Seonu <seonup95@gmail.com>
2024-01-03 19:59:40 +0900

    refactor: SublistItem 컴포넌트 구조 변경
    - Emotion의 css 함수 제거
    - customStyle props 제거
    - titleAs 타입 좁히기
    - Title 스타일 추가

--- src/components/common/atoms/SublistItem.tsx
+++ src/components/common/atoms/SublistItem.tsx
@@ -1,47 +1,51 @@
-import { css } from '@emotion/react';
 import { colors } from '../../../styles/variables/colors';
-import { ElementType, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { BiSubdirectoryRight } from 'react-icons/bi';
-import { Style } from '../../../types/Style';
+import Title from './Title';
 
-interface Props {
+export interface Props {
   title: string;
-  titleAs: ElementType;
-  customStyle?: Style;
+  titleAs?: 'h3' | 'h4' | 'h5';
   children?: ReactNode;
 }
 
-export default function SublistItem({ title, titleAs: Title, customStyle, children }: Props) {
+export default function SublistItem({ title, titleAs, children }: Props) {
   return (
     <div
-      css={css(
-        {
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          gap: '16px',
-          fontSize: '12px',
-          fontWeight: 400,
-          color: colors.DARK_GRAY,
-        },
-        customStyle,
-      )}
+      css={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: '16px',
+        fontSize: '12px',
+        fontWeight: 400,
+        color: colors.DARK_GRAY,
+      }}
     >
       <div
-        css={css({
+        css={{
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
           gap: '4px',
-        })}
+        }}
       >
         <BiSubdirectoryRight
-          css={css({
+          css={{
             fontSize: '14px',
             color: colors.LIGHT_GRAY,
-          })}
+          }}
         />
-        <Title>{title}</Title>
+        <Title
+          as={titleAs || 'h4'}
+          css={{
+            fontSize: '12px',
+            fontWeight: 500,
+            color: colors.DARK_GRAY,
+          }}
+        >
+          {title}
+        </Title>
       </div>
 
       {children}
