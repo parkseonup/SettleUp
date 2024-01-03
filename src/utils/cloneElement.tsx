0fbdc2bba9c576c0831727c787c233b3992584af
Seonu <seonup95@gmail.com>
2024-01-03 20:06:10 +0900

    fix: cloneElement시 ref 속성 전달
    문제 상황
    - 커스텀 cloneElement로 element를 clone할 때 ref 속성값이 복사되지 않는 문제 발생
    
    문제 원인
    - jsx 함수로 clone할 때 ref 속성을 따로 전달하지 않아서 발생된 문제
    
    해결 방안
    - jsx 함수로 clone할 때 두번째 인수에 ref 속성을 추가해줌
    - 단, ref가 없을 경우가 있기 때문에 원본 element에 ref 속성이 있는지 확읺나는 작업을 해줘야 타입스크립트 에러가 발생하지 않음

--- src/utils/cloneElement.tsx
+++ src/utils/cloneElement.tsx
@@ -1,15 +1,28 @@
 import { jsx } from '@emotion/react';
-import { ReactElement, cloneElement as cloneReactElement } from 'react';
+import { ReactElement, ReactNode, cloneElement as cloneReactElement } from 'react';
 
-export default function cloneElement(element: ReactElement, props: any): ReactElement {
+export default function cloneElement(
+  element: ReactElement,
+  props?: any,
+  ...children: ReactNode[]
+): ReactElement {
   return element.props.css
-    ? cloneReactElement(element, {
-        ...element.props,
-        ...props,
-      })
-    : jsx(element.type, {
-        key: element.key,
-        ...element.props,
-        ...props,
-      });
+    ? cloneReactElement(
+        element,
+        {
+          ...element.props,
+          ...props,
+        },
+        ...children,
+      )
+    : jsx(
+        element.type,
+        {
+          key: element.key,
+          ref: 'ref' in element ? element.ref : null,
+          ...element.props,
+          ...props,
+        },
+        ...children,
+      );
 }
