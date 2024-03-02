939af65f6e0562ea729c11e571317d35d93cb183
Seonu <seonup95@gmail.com>
2024-03-02 18:03:29 +0900

    chore: 기획 변경으로 msw 제거

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-async function enableMocking() {
-  if (import.meta.env.MODE !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/browser');
-
-  return worker.start();
-}
-
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
