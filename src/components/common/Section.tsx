67117823b2936748922817991c351ed0bc4cea9b
Seonu <seonup95@gmail.com>
2024-03-02 18:08:35 +0900

    feat: underline style 모드 추가

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,15 +1,39 @@
 import { ReactNode } from 'react';
 import Title from './Title';
+import { Style } from '../../types/Style';
+import { colors } from '../../styles/variables/colors';
+import { css } from '@emotion/react';
 
 interface Props {
   title: string;
+  type?: 'default' | 'underline';
+  insideStyle?: Style;
   children: ReactNode;
 }
 
-export default function Section({ title, children }: Props) {
+const underlineStyle = {
+  paddingBottom: '16px',
+  borderBottom: `1px solid ${colors.BLACK}`,
+};
+
+export default function Section({
+  title,
+  type = 'default',
+  insideStyle,
+  children,
+}: Props) {
   return (
-    <section>
-      <Title as="h3" font="size100" css={{ marginBottom: '16px' }}>
+    <section css={insideStyle}>
+      <Title
+        as="h3"
+        font="size100"
+        css={css(
+          {
+            marginBottom: '16px',
+          },
+          type === 'underline' ? underlineStyle : null,
+        )}
+      >
         {title}
       </Title>
 
