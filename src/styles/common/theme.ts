024de054b499ad027efbb18f493216d7fd38b21b
Seonu <seonup95@gmail.com>
2024-03-21 18:14:00 +0900

    refactor: 762aba7 커밋 누락 파일 추가

---
+++ src/styles/common/theme.ts
@@ -0,0 +1,12 @@
+import { colors } from '../variables/colors';
+
+export const theme = {
+  default: {
+    color: colors.BLACK,
+    backgroundColor: colors.WHITE,
+  },
+  point: {
+    color: colors.WHITE,
+    backgroundColor: colors.POINT,
+  },
+};
