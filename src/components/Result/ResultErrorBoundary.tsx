1623da805931d0143b950ec6a1d44151a3e926b9
Seonu <seonup95@gmail.com>
2024-03-22 14:11:06 +0900

    refactor: css 함수로 교체,  description 변경

--- src/components/Result/ResultErrorBoundary.tsx
+++ src/components/Result/ResultErrorBoundary.tsx
@@ -1,17 +1,15 @@
 import PageLayout from '../common/pageLayout/PageLayout';
 import { Link } from 'react-router-dom';
 import { buttonColors, defaultButtonStyle } from '../../styles/common/buttons';
+import { css } from '@emotion/react';
 
 export default function ResultErrorBoundary() {
   return (
-    <PageLayout title="정산 결과가 없습니다." description="생성된 정산 내역이 없습니다.">
-      <Link
-        css={{
-          ...defaultButtonStyle,
-          ...buttonColors['fill'],
-        }}
-        to="/create"
-      >
+    <PageLayout
+      title="정산 결과가 없습니다."
+      description="정산 만들기를 먼저 진행해주세요."
+    >
+      <Link css={css(defaultButtonStyle, buttonColors['fill'])} to="/create">
         정산 만들기로 이동
       </Link>
     </PageLayout>
